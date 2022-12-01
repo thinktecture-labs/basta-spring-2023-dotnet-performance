37bb1c91632147d48daffe5ba69b0cef8c84f96f
Sebastian Gingter <sebastian.gingter@thinktecture.com>
2022-12-01 10:46:14 +0100

    add benchmark sample project

---
+++ src/Benchmark/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Text;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+BenchmarkRunner.Run<Sample>();
+
+public class Sample
+{
+    [Benchmark]
+    public string SoDomething()
+    {
+        var sb = new StringBuilder();
+
+        for (var i = 0; i < 10_000; i++)
+        {
+            sb.AppendLine($"Adding {i}");
+        }
+
+        return sb.ToString();
+    }
+}
